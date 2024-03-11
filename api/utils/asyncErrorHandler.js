233a585e6c4a55b9f77ca45e550ad2e4c8e5c744
argon-3467 <akhildhatterwal@gmail.com>
2024-03-11 20:59:02 +0530

    Add express-async-handler library
    - More robust and types :) than my own implementation

--- api/utils/asyncErrorHandler.js
+++ api/utils/asyncErrorHandler.js
@@ -1,17 +1,3 @@
-/**
- * Asynchronous error handler.
- * Wraps a function in a try-catch block to handle rejected promises.
- *
- * @param {Function} fn - The asynchronous function to wrap.
- * @returns {Function} - A new function that catches any errors thrown by the original function and passes them to the next middleware.
- *
- * @example
- * app.get('/', asyncErrorHandler(async (req, res, next) => {
- *   // Your code here...
- * }));
- */
-export default function asyncErrorHandler(fn) {
-  return function (req, res, next) {
-    fn(req, res, next).catch(next);
-  };
-}
+import asyncErrorHandler from "express-async-handler";
+
+export default asyncErrorHandler;
